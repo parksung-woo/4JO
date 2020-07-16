038ed3e6d156b86b6e8ca8e729cffc40992a4128
송주현 <songjh2789@gamil.com>
2020-07-16 14:49:55 +0900

    ui test01

---
+++ team4/app/src/main/java/com/example/team4/acticity_main2.kt
@@ -0,0 +1,11 @@
+package com.example.team4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class acticity_main2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
