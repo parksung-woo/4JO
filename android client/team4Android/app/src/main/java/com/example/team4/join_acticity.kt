fc50c00bae23cbb4b35e20a03f72210c87f6afc5
박성우 <tjddn10123@gmail.com>
2020-07-24 17:53:37 +0900

    회원가입시 비밀번호와 비밀번호확인 처리

--- android client/team4Android/app/src/main/java/com/example/team4/join_acticity.kt
+++ android client/team4Android/app/src/main/java/com/example/team4/join_acticity.kt
@@ -14,12 +14,16 @@ class join_acticity : AppCompatActivity() {
         setContentView(R.layout.activity_join)
         //회원가입 완료 버튼
         button_signup.setOnClickListener {
+            if(edit_name2 == edit_name3){
             Toast.makeText(this, "회원가입을 성공했습니다."
                 , Toast.LENGTH_SHORT).show()
             val intent = Intent(this, login_activity::class.java) //LIstPageActivity로 이동
 //            intent.putExtra("id",user_id.text.toString())
 //            Log.i("testLog user_id -> ", user_id.text.toString())
             startActivity(intent)
+            }else
+                Toast.makeText(this, "비밀번호를 확인하세요."
+                    , Toast.LENGTH_SHORT).show()
         }
     }
 }
