8050f43ffef64b68bc76b28ec1de07d4d6a6567d
조동연 <jodong7924yeon@gmail.com>
2020-08-07 15:11:19 +0900

    Revert "아이콘 클릭시 인사말"
    This reverts commit e8bedc696d46473dd0ed447611419fd24a4a24e2.

--- android client/team4Android/app/src/main/java/com/example/team4/management_activity.kt
+++ android client/team4Android/app/src/main/java/com/example/team4/management_activity.kt
@@ -3,8 +3,6 @@ package com.example.team4
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_management.*
 
 class management_activity : AppCompatActivity() {
@@ -28,13 +26,6 @@ class management_activity : AppCompatActivity() {
         graphview.setOnClickListener {
             val intent = Intent(this, graph_activity::class.java)
             startActivity(intent)
-        }
-        //이미지 버튼 클릭시 인사
-        imageView7.setOnClickListener {
-            Toast.makeText(
-                this, "안녕!!!!!!!!!!!!!"
-                , Toast.LENGTH_SHORT
-            ).show()
         }
     }
 }
